bf1e9febd49548b1e2b0c7eaa5b26435224d8d83
yunnysunny <yunnysunny@gmail.com>
2022-09-06 16:01:19 +0800

    fix: using bash to run sed command

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM centos:7
 
+SHELL ["/bin/bash", "-c"]
 # 更改 yum 源
 RUN sed -e 's|^mirrorlist=|#mirrorlist=|g' \
          -e 's|^#baseurl=http://mirror.centos.org/centos|baseurl=https://mirrors.ustc.edu.cn/centos|g' \
--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ TAG_LATEST=yunnysunny/centos:latest
 TAG_CURRENT=yunnysunny/centos:${VERSION}
 
 docker pull centos:${VERSION}
-docker build . -f ./Dockerfile -t ${TAG_LATEST} -t ${TAG_CURRENT}
+docker build . -f ./Dockerfile -t ${TAG_LATEST} -t ${TAG_CURRENT} --progress=plain
 if [ "$NEED_PUSH" = "1" ] ; then
     docker push ${TAG_LATEST}
     docker push ${TAG_CURRENT}
